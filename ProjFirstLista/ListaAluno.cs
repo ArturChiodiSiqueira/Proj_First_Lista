6a916cc703519a4fd6e5d36c22c7210cd35887b6
artur <artur@DESKTOP-V0JU5PP>
2022-09-05 13:59:45 -0300

    Add project files.

---
+++ ProjFirstLista/ListaAluno.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjFirstLista
+{
+    internal class ListaAluno
+    {
+        public Aluno HEAD { get; set; }
+        public Aluno TAIL { get; set; }
+
+        public ListaAluno()
+        {
+            HEAD = TAIL = null;
+        }
+
+        public bool Vazia()
+        {
+            if ((HEAD == null) && (TAIL == null))
+                return true;
+            else
+                return false;
+        }
+
+        public void Print()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("impossivel imprimir, Lista Vazia!");
+            }
+            else
+            {
+                Aluno auxiliar = HEAD;
+
+                Console.WriteLine("DADOS DA LISTA DE ALUNOS");
+                do
+                {
+                    Console.WriteLine("\n" + auxiliar.ToString() + "\n");
+                    auxiliar = auxiliar.Proximo;
+                } while (auxiliar != null);
+                Console.WriteLine("FIM DA IMPRESSÃO.\n");
+            }
+            Console.ReadKey();
+        }
+
+        public void Push(Aluno auxiliar)
+        {
+            if (Vazia())                                     //inserção lista vazia
+            {
+                HEAD = TAIL = auxiliar;
+            }
+                                                             //inserção apos a tail (novo ultimo elemento)
+            else                                             //lembrando que a inserção deve seguir uma ordenação
+            {
+                if (auxiliar.Nome.CompareTo(TAIL.Nome) >= 0) //neste exemplo faremos a ordem alfabetica por nome do aluno
+                {
+                    TAIL.Proximo = auxiliar;                 //inserido novo objeto como ultimo - encadeamento
+                    TAIL = auxiliar;                         //ajustando a tail para o novo ultimo elemento
+                }
+                                                             //inserção antes do head (novo primeiro elemento)
+                else
+                {
+                    if (auxiliar.Nome.CompareTo(HEAD.Nome) < 0)
+                    {
+                        auxiliar.Proximo = HEAD;             //faco o encadeamento do novo primeiro elemento na lista
+                        HEAD = auxiliar;                     //ajustando a head para o novo primeiro elemento da lista
+                    }
+
+                    else                                     //incersao no meio da lista
+                    {
+                        Aluno aux1, aux2;
+                        aux1 = HEAD;
+                        aux2 = HEAD;
+
+                        do
+                        {
+                            if (auxiliar.Nome.CompareTo(aux1.Nome) >= 0)
+                            {                                //procurando a posição para inserir o novo objeto
+                                aux2 = aux1;                 //controlar o anterior
+                                aux1 = aux1.Proximo;         //controlar o proximo
+                            }
+                            else                             //posso inserir o novo objeto na lista
+                            {
+                                aux2.Proximo = auxiliar;     //encadeamento do objeto anterior
+                                auxiliar.Proximo = aux1;     //encadeamento do objeto proximo
+                                break;
+                            }
+                        } while (true);
+                    }
+                }
+            }
+        }
+
+        public void pop(string nomeRemovido)
+        {
+            if (Vazia())
+                Console.WriteLine("Lista Vazia! Impossivel remover.");
+            else
+            {
+                HEAD = HEAD.Proximo;
+                Console.WriteLine("Aluno: " + nomeRemovido + " removido!");
+            }
+            Console.WriteLine("\nAperte qualquer coisa para seguir.");
+            Console.ReadKey();
+        }
+
+        public void Length()
+        {
+            int contador = 0;
+            if (Vazia())
+                Console.WriteLine("Lista Vazia!");
+            else
+            {
+                Aluno auxiliar = HEAD;
+                do
+                {
+                    contador++;
+                    auxiliar = auxiliar.Proximo;
+
+                } while (auxiliar != null);
+                Console.WriteLine("A Lista tem " + contador + " aluno(s).");
+            }
+            Console.ReadKey();
+        }
+
+        public void Find(string nome)
+        {
+            if (Vazia())
+                Console.WriteLine("Lista Vazia!");
+            else
+            {
+                Aluno auxiliar = HEAD;
+                bool achou = false;
+                do
+                {
+                    if (auxiliar.Nome == nome)
+                    {
+                        Console.WriteLine("Aluno localizado!\n");
+                        Console.WriteLine(auxiliar.ToString());
+                        Console.WriteLine("\n");
+                        achou = true;
+                    }
+                    auxiliar = auxiliar.Proximo;
+                } while (auxiliar != null);
+                if (!achou)
+                    Console.WriteLine("Aluno nao localizado com esse nome: " + nome + "\n");
+                else
+                    Console.WriteLine("\nFim da busca.");
+
+                Console.WriteLine("\nAperte qualquer coisa para seguir.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ ProjFirstLista/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace ProjFirstLista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaAluno MinhaLista = new ListaAluno();
+            MinhaLista.Push(new Aluno(123, "Baratão", 34, "Turismo"));
+            MinhaLista.Push(new Aluno(124, "Baratão", 44, "Engenharia Civil"));
+            MinhaLista.Push(new Aluno(456, "Moranguinho", 27, "Agronomia"));
+            MinhaLista.Push(new Aluno(345, "Celso Xaropinho", 33, "Telemarketing"));
+            MinhaLista.Push(new Aluno(777, "Artur", 84, "Administração"));
+            MinhaLista.Push(new Aluno(435, "Mussarela", 22, "Veterinária"));
+            MinhaLista.Push(new Aluno(129, "Alexandre", 55, "Eletrica"));
+            MinhaLista.Push(new Aluno(999, "Celso Xaropinho", 66, "Pos - Telemarketing"));
+
+            MinhaLista.Print();
+
+            MinhaLista.Length();
+
+            Console.Clear();
+
+            Console.Write("\nInforme o nome que deseja localizar: ");
+            string nome = Console.ReadLine();
+            MinhaLista.Find(nome);
+
+            Console.Write("\nInforme o nome que deseja remover: ");
+            string nomeremovido = Console.ReadLine();
+            MinhaLista.pop(nomeremovido);
+
+            Console.Clear();
+
+            MinhaLista.Print();
+
+            MinhaLista.Length();
+        }
+    }
+}
